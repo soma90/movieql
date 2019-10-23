dd50c2f17b145412a7fcb6939792844c193bed3a
kyutae <soma90-1@hanmail.net>
2019-10-23 11:14:07 +0900

    wrapping a REST API with GraphQL Part Two

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,17 +1,12 @@
 //resolver - query 에 대한 작업
 
-import { getMovies, getById, addMovie, deleteMovie } from "./db";
+import { getMovies } from "./db";
 
 const resolvers = {
   Query: {
     //name이라는 쿼리를 보냈을때
     //name: () => "soma"
-    movies: () => getMovies(),
-    movie: (_, { id }) => getById(id)
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id)
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
   }
 }
 
